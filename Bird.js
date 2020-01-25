4e3b41ee9fe6be074ebeb9ef9aad0f4f76a44686
ilan M <ilanm@ilans-iMac.local>
2020-01-25 12:38:30 +0530

    array

--- Bird.js
+++ Bird.js
@@ -2,6 +2,8 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.tragetory =[];
   }
 
   display() {
@@ -9,5 +11,12 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    if(this.body.velocity.x>10 && this.body.position.x>200){
+    var pos =[this.body.position.x,this.body.position.y];
+     this.tragetory.push(pos);
+    }
+     for(var i = 0;i<this.tragetory.length;i++){
+       image(this.smokeImage,this.tragetory[i][0],this.tragetory[i][1]);
+     }
   }
 }
